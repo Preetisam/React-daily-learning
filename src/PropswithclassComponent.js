2a3117d7b179f666d03e455bf61717197785a637
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2023-02-25 23:32:28 +0530

    Update and rename app14.js to PropswithclassComponent.js

--- src/PropswithclassComponent.js
+++ src/PropswithclassComponent.js
@@ -3,24 +3,24 @@ import React from "react";
 import "./App.css";
 import Classmet from "./Classmet";
 //Props with class Component
-// function App() {
+function App() {
 
-//   return (
-//     <div className="App">
-//       <h1>Props !</h1>
-//       <Classmet name="preeti" email="sam@test.com"></Classmet>
-//       <Classmet name="grace" email="grace@test.com"></Classmet>
-//       <button>Update Name</button>
-//     </div>
-//   );
-// }
-// export default App;
+  return (
+    <div className="App">
+      <h1>Props !</h1>
+      <Classmet name="preeti" email="sam@test.com"></Classmet>
+      <Classmet name="grace" email="grace@test.com"></Classmet>
+      <button>Update Name</button>
+    </div>
+  );
+}
+export default App;
 
 //can i change the props in the place we recive it
 //we can change the props when we send in the parent component.
 
 //we can cant change in the child componet where we are using it either in functional component  or class component
-
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 class App extends React.Component {
   //to define the state we use constructor to define inside it
   constructor() {
